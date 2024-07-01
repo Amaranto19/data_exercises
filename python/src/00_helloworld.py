176d6ca793274518acfb6595870e60d2ae599990
Amaranto <blasquez1999@gmail.com>
2024-07-01 18:38:33 +0200

    hOLA COMMIT

---
+++ python/src/00_helloworld.py
@@ -0,0 +1,12 @@
+entero: int = 37
+cadena: str = 'cadena cualquiera'
+buleano: bool = True
+flut: float = 3.14159265
+
+print(type(entero))
+print(type(cadena))
+print(type(buleano))
+print(type(flut))
+
+buleano:bool =str(buleano)
+print(type(buleano))
